278adac78a99db2b72e840c8548daa2670aea12c
miebakso <miebaksoz@gmail.com>
2020-09-23 22:34:57 +0700

    finish table head and overview

--- iris_petal/analysis/views.py
+++ iris_petal/analysis/views.py
@@ -7,8 +7,8 @@ import os.path, requests, logging
 logger = logging.getLogger(__name__)
 
 def index(request):
-    context = {'test':10}
-    url =  settings.MEDIA_ROOT + '/iris.csv'
+    context = {}
+    url = settings.MEDIA_ROOT + '/iris.csv'
 
     # Download and store file in DIR if not exist 
     if(not os.path.isfile(url)):
@@ -16,18 +16,39 @@ def index(request):
         r = requests.get(iris_url, allow_redirects=True)
         open(url, 'w').write(r.text)
         logger.info('berhasil download')
-        
+
     # iris data set
     names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
     dataset = read_csv(url, names=names)
-    context['head'] = dataset.head().to_json()
-    logger.info(dataset.head())
-    logger.info(type(dataset))
-    logger.info(context['head'])
 
-    #print(settings.MEDIA_ROOT+"ASD")
-    #path = default_storage.save('iris.csv', dataset.to_csv('iris.csv'))
-    #print(path)
-    #default_storage.save('iris.csv', dataset.to_csv('iris.csv'))
-    #file_name = default_storage.save('iris.csv', dataset.to_csv('iris.csv'))
+    # data set description
+    shape = dataset.shape
+    context['total_elements'] = shape[0]
+    context['total_attributes'] = shape[1]
+    context['labels'] = names
+
+    # data head()
+    datahead = []
+    temp = dataset.head(10)
+    i = 0
+    for x in temp:
+        j = 0
+        while j < len(temp[x]):
+            if i < len(temp):
+                datahead.append([])
+                datahead[j].append(temp[x][j])
+                i+=1
+            else:
+                print(x)
+                datahead[j].append(temp[x][j])
+            j+=1
+
+        
+    context['head'] = datahead
+    print(temp)
+    
+    #logger.info(dataset.head())
+    #logger.info(type(dataset))
+    #logger.info(context['head'])
+
     return render(request, 'iris/index.html', context)
